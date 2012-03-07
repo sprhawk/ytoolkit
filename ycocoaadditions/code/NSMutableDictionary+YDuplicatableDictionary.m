cea2b9a71f42cc40be2c9afa572b8a3177a4ca80
sprhawk <hongbo@yang.me>
2012-03-07 15:53:21 +0800

    will not throw an execption when argument is nil

--- ycocoaadditions/code/NSMutableDictionary+YDuplicatableDictionary.m
+++ ycocoaadditions/code/NSMutableDictionary+YDuplicatableDictionary.m
@@ -76,9 +76,6 @@
     }
 }
 - (void)addDuplicatableEntriesFromDictionary:(NSDictionary *)otherDictionary {
-    if (nil == otherDictionary) {
-        @throw NSInvalidArgumentException;
-    }
     for (id key in otherDictionary) {
         id value = [otherDictionary objectForKey:key];
         [self addDuplicatableObject:value forKey:key];
