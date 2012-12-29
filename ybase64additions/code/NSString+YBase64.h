97bd5501a87e43de11feee0fbeb37811e459aa76
sprhawk <hongbo@yang.me>
2012-12-29 22:48:16 +0800

    修改了oauth2登录

---
+++ ybase64additions/code/NSString+YBase64.h
@@ -0,0 +1,13 @@
+//
+//  NSString+YBase64.h
+//  ytoolkit
+//
+//  Created by YANG HONGBO on 2012-12-29.
+//  Copyright (c) 2012年 Douban Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (YBase64)
+- (NSString *)base64;
+@end
---
+++ ybase64additions/code/NSString+YBase64.m
@@ -0,0 +1,26 @@
+//
+//  NSString+YBase64.m
+//  ytoolkit
+//
+//  Created by YANG HONGBO on 2012-12-29.
+//  Copyright (c) 2012年 Douban Inc. All rights reserved.
+//
+
+#import "NSString+YBase64.h"
+#import "ybase64.h"
+
+@implementation NSString (YBase64)
+- (NSString *)base64
+{
+    const char * string = [self cStringUsingEncoding:NSUTF8StringEncoding];
+    size_t len = strlen(string);
+    size_t size = ybase64_decode(string, len, NULL, 0);
+    char * newstring = (char *)malloc(size + 1);
+    memset(newstring, 0, size + 1);
+    len = ybase64_decode(string, len, newstring, size);
+    NSString * nsstr = [NSString stringWithCString:newstring encoding:NSUTF8StringEncoding];
+    free(newstring);
+    newstring = NULL;
+    return nsstr;
+}
+@end
